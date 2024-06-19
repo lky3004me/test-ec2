149a89448f23b8d9d766a5b9a6a1cad7d70c2af4
lky3004me <lky3004me@gmail.com>
2024-06-19 15:11:11 +0900

    first

---
+++ src/main/java/com/example/test_ec2/TestEc2Application.java
@@ -0,0 +1,13 @@
+package com.example.test_ec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_ec2/TestEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_ec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
